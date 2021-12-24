f349ea535595d7ebdad02c33a2ed22a8cdd0e80e
Zach Caldwell <zach.caldwell101@gmail.com>
2021-12-24 00:09:55 -0700

    Implement Modal and Button components

---
+++ src/components/bootstrap/BButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <button type="button" :class="`btn btn-${color} ${classes}`" :data-bs-target="target" :data-bs-toggle="toggle" :data-bs-dismiss="dismiss"><slot>Button</slot></button>
+</template>
+
+<script>
+import 'bootstrap';
+
+export default {
+  name: 'BButton',
+  props: {
+    color: {
+      type:String,
+      default: 'primary'
+    },
+    classes: {
+      type:String,
+      default: ''
+    },
+    target: {
+      type:String,
+      default: ''
+    },
+    toggle: {
+      type:String,
+      default: ''
+    },
+    dismiss: {
+      type:String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/bootstrap/BModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="modal" :id="id" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"><slot name="title">{{ title }}</slot></h5>
+                    <BButton color="close" dismiss="modal" @click="$emit('cancel')"><span aria-hidden="true">&times;</span></BButton>
+                </div>
+                <div class="modal-body">
+                    <p><slot name="content">{{ content }}</slot></p>
+                </div>
+                <div class="modal-footer">
+                    <BButton color="secondary" dismiss="modal" @click="$emit('cancel')"><slot name="cancelLabel">{{ cancelLabel }}</slot></BButton>
+                    <BButton dismiss="modal" @click="$emit('confirm', this)"><slot name="confirmLabel">{{ confirmLabel }}</slot></BButton>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import 'bootstrap';
+import BButton from './BButton.vue'
+
+export default {
+  name: 'BModal',
+  components: {
+    BButton
+  },
+  emits: ['confirm', 'cancel'],
+  props: {
+    id: {
+      type:String,
+      default: ''
+    },
+    title: {
+      type:String,
+      default: 'Please Confirm'
+    },
+    content: {
+      type:String,
+      default: 'Do you want to continue?'
+    },
+    cancelLabel: {
+      type:String,
+      default: 'Cancel'
+    },
+    confirmLabel: {
+      type:String,
+      default: 'Confirm'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
